4c52e8970d633764370dcf4d8ac0cbc106428888
Jonathan Gonzalez <jgonzalez249@gmail.com>
2023-02-18 04:25:16 -0500

    Updated Generation/Refactor

--- generateText.py
+++ generateText.py
@@ -1,13 +1,18 @@
 # IF NOT INSTALLED:
-#!pip install -q gpt-2-simple
-#!pip install tracery
-#!pip install tensorflow (tensorflow-gpu if you have a GPU)
-
+# pip install -q gpt-2-simple
+# pip install tracery
+# pip install tensorflow (tensorflow-gpu if you have a GPU)
+# pip install nltk
 
 import gpt_2_simple as gpt2
 import tracery
+#import tracery_grammar
 import random
 import re
+import nltk
+
+nltk.download('punkt')
+from nltk.tokenize import sent_tokenize
 
 
 from tracery.modifiers import base_english
@@ -24,33 +29,24 @@ gpt2.load_gpt2(sess, checkpoint_dir=pretrained_model_dir)
 # Tracery grammar for the story
 grammar = tracery.Grammar({
     "story": [
-    "#hero.capitalize# wakes up in #location# and discovers #discovery#",
-    "In a world where #technology# is #adjective#, #hero.capitalize# must find a way to #goal# before it's too late",
-    "#hero.capitalize# travels forward in time to #year# and is surprised to find they #surprise#",
-    "After a mission to explore a distant #planet#, #hero.capitalize# discovers a #alien# that changes everything",
-    "In a future where #disaster# has led to widespread #adjectiveSituation#, #hero.capitalize# must #mission# to #goal# and #survive#",
-    "In the year #year#, #hero.capitalize# discovers a new form of #technology# that alters the course of #history# #adverb#",
-    "After a freak accident, A #hero.capitalize# gains the power of #superpower# and must use it to #responsibility#",
-    "The government has been experimenting with #technology# for years, but #hero.capitalize# discovers the #adjective# truth #conspiracy#",
-    "#hero.capitalize# is a #occupation# who discovers a plot to #conspiracy# and must stop it before it's too late",
-    "In a world where #humans# are #adjective#, #hero.capitalize# must navigate a dangerous #dilemma# to #survive#",
-    "#hero.capitalize# wakes up in a #location# and discovers that they are the last person alive",
-    "In a world where #technology# can manipulate reality, #hero.capitalize# must uncover the conspiracy to control #goal#",
-    "#hero.capitalize# travels to a distant #planet# in search of #discovery# but ends up in the middle of a #disaster#",
-    "After a mission to investigate an abandoned facility, #hero.capitalize# #surprise.s# that could destroy the world",
-    "In a future where #technology# has advanced beyond imagination, #hero.capitalize# must #mission# to prevent #disaster#",
-    "In the year #year#, #hero.capitalize# is tasked with building a #technology# technology that could change the world #adverb#",
-    "After a lab accident, #hero.capitalize# gains the power of #superpower# but quickly realizes it comes at a terrible cost",
-    "The government has been covering up the existence of #alien# for years, but #hero.capitalize# is about to uncover the #adjective# truth",
-    "#hero.capitalize# is a #occupation# who discovers a hidden society of #humans# with incredible #superpower#",
-    "In a world where humans are #humans# and are no longer the dominant species, #hero.capitalize# must adapt or #challenge#",
-    "#hero.capitalize# wakes up in a parallel universe where everything they know is #adjective#, and they must find a way home",
-    "In a world where #technology# can rewrite memories, #hero.capitalize# must use their skills as a #occupation# to uncover the #conspiracy#",
-    "#hero.capitalize# travels to the distant future and finds a world that is both #adjective# and #adjective#",
-    "After a mission to retrieve a powerful artifact, #hero.capitalize# is betrayed by their team and must find a way to #survive#",
-],
+        "#hero.capitalize# #action# #modifier#, #discovery#",
+        "#hero.capitalize# #action# #modifier# and must #goal# before it's too late",
+        "#hero.capitalize# #action# #modifier# to #surprise#",
+        "#hero.capitalize# #action# #modifier# on a mission to #goal# and #survive#",
+        "#hero.capitalize# #action# #modifier# and discovers they are the last person alive",
+        "#hero.capitalize# #action# #modifier# in a world where #challenge#",
+        "#hero.capitalize# #action# #modifier# and must uncover the conspiracy to control #goal#",
+        "#hero.capitalize# #action# #modifier# and finds a world that is both #adjective# and #adjective#",
+        "#hero.capitalize# #action# #modifier# and must find a way to #survive#",
+        "#hero.capitalize# #action# #modifier# and discovers a hidden society of #humans# with incredible #superpower#",
+        "#hero.capitalize# #action# #modifier# in a world where humans are #humans# and are no longer the dominant species, #hero.capitalize# must adapt or #challenge#",
+        "#hero.capitalize# #action# #modifier# in a parallel universe where everything they know is #adjective#, and they must find a way home",
+        "#hero.capitalize# #action# #modifier# and must use their skills as a #occupation# to uncover the #conspiracy#",
+    ],
     "hero": ["Aaliyah", "Aiden", "Akira", "Alara", "Alexa", "Alexios", "Alyssa", "Amara", "Anakin", "Andromeda", "Apollo", "Aria", "Aric", "Arya", "Atlas", "Aurora", "Avril", "Ayana", "Azura", "Ben", "Calypso", "Cameron", "Cassius", "Celeste", "Ceres", "Chandra", "Chara", "Chris", "Cleo", "Cygnus", "Dahlia", "Darian", "Dash", "Demeter", "Dione", "Dominic", "Drake", "Eris", "Esmeralda", "Ethan", "Evangeline", "Gaia", "Gideon", "Halcyon", "Harper", "Hestia", "Icarus", "Io", "Jada", "Jaxon", "Kaida", "Kalen", "Kalliope", "Kamari", "Kato", "Kenji", "Kian", "Korbin", "Kyra", "Landon", "Lavinia", "Leo", "Lysandra", "Lystra", "Mace", "Maia", "Mako", "Marcella", "Mars", "Micah", "Mira", "Nadia", "Neo", "Nova", "Orion", "Pandora", "Phoenix", "Qadira", "Rhea", "Rigel", "Saffron", "Sage", "Samara", "Selena", "Serena", "Sirius", "Sol", "Stella", "Tahlia", "Talos", "Tara", "Terra", "Thalia", "Thetis", "Titania", "Triton", "Ulysses", "Vega", "Venus", "Xander", "Xanthe", "Yara", "Zara", "Zephyr", "Zora"],
     "location": ["on a space station orbiting a black hole", "in a city on a planet with a toxic atmosphere", "in an underground research facility on a distant moon", "on a terraformed asteroid colony", "in a cyberpunk megacity", "on a generation ship traveling through deep space", "in a virtual reality simulation", "on a planet overrun by alien vegetation", "in a post-apocalyptic wasteland", "in a time-traveling space vessel", "in a floating city above the clouds", "in a post-apocalyptic wasteland", "on a space station orbiting a black hole", "in a virtual reality simulation", "on a terraformed moon of #planet#"],
+    "action": ["wakes up", "travels forward in time", "travels to a distant planet", "gains the power of", "is tasked with building", "is a"],
+    "modifier": ["#superpower# #location#", "after a mission to explore #planet#", "after a mission to investigate an abandoned facility", "in the year #year#", "in a future where #disaster#", "in a world where #technology#", "after a freak accident"],
     "discovery": ["a new species of alien life", "a hidden government conspiracy", "a rogue artificial intelligence", "the secret to time travel", "a cure for a deadly disease"],
     "technology": ["robotics", "holography", "teleportation", "time travel", "antigravity", "cloning", "stem cells", "nanobots", "superconductors", "lasers", "space travel", "terraforming", "warp drive", "wormholes", "dyson spheres", "mind uploading", "brain-computer interface", "simulated reality", "digital immortality", "mind control"],
     "adjectiveSituation": ["out of control", "the key to salvation", "banned by the government", "beyond human comprehension", "on the brink of collapse", "under investigation", "shrouded in mystery", "plagued by corruption", "in need of reform", "threatened by a new enemy", "ripe for revolution", "haunted by a dark past", "fueled by greed and ambition", "inspired by a noble cause", "ravaged by a deadly virus", "dominated by a powerful faction", "torn apart by civil war", "protected by a secret order", "infested with zombies and mutants", "infiltrated by spies and traitors","dependent on a scarce resource","governed by a strict code of honor","exposed to a cosmic anomaly","at war with an alien race","facing an imminent disaster"],
@@ -77,37 +73,40 @@ grammar.add_modifiers(base_english)
 # Set the number of chucks to generate (1024 Tokens each)
 num_chunks = 3
 chunk_size = 1024 # 1024 is the maximum number of tokens the model can generate at a time
-chunks = num_chunks  
-gen_length = chunk_size * chunks # Total number of tokens to generate
+total_chunks = num_chunks  
+gen_length = chunk_size * total_chunks # Total number of tokens to generate
 
 temp = 1 # 0.7 - 1.0 is recommended range
 top_k = 15 # 0 - 40 is recommended range
 top_p = 0.9 # 0.9 is recommended
 
 text = ""
-prefix = grammar.flatten("#story#")  # initialize prefix outside of loop
-for i in range(chunks):
-    
+prefix = grammar.flatten("#story#")  # initialize prefix
+chunks = []
+for i in range(total_chunks):
+    # Include prefix for first chunk
     if i == 0:
-        # Generate text using the model and the prefix from above
-        chunk = gpt2.generate(sess, run_name='run1', prefix=prefix, include_prefix=False, length=chunk_size, temperature=temp, top_k=top_k, top_p=top_p, return_as_list=True)[0]
+        chunk_prefix = prefix
     else:
-        # Generate text using the model and the last sentence of the previous chunk as prefix
-        sentences = re.split("(?<=[.!?]) +", text)
-        prefix = sentences[-1] if len(sentences) > 1 else prefix
-        chunk = gpt2.generate(sess, run_name='run1', prefix=prefix, include_prefix=False, length=chunk_size, temperature=temp, top_k=top_k, top_p=top_p, return_as_list=True)[0]
-    
+        # Use the last 3 sentences of the previous chunk as prefix
+        last_sentences = sent_tokenize(chunks[-1])[-3:]
+        chunk_prefix = ' '.join(last_sentences)
+
+    # Generate text using the model and the prefix from above
+    chunk = gpt2.generate(sess, run_name='run1', prefix=chunk_prefix, include_prefix=False, length=chunk_size, temperature=temp, top_k=top_k, top_p=top_p, return_as_list=True, truncate<|endoftext|>)[0]
+
     if chunk is not None:
-        text += chunk
+        chunks.append(chunk)
         # remove the prefix from the beginning of the chunk
-        sentences = re.split("(?<=[.!?]) +", chunk)
-        prefix = sentences[-1] if len(sentences) > 1 else chunk
+        last_sentences = sent_tokenize(chunk)[-3:]
+        prefix = ' '.join(last_sentences)
         print(chunk)
     else:
         print("Error: generated text is None.")
     
     # Print progress
-    print("\033[1mGenerated chunk {}/{}\033[0m".format(i+1, chunks))
+    #print(f"\033[1mGenerated chunk {i+1}/{total_chunks}\033[0m")
     
-# Print the generated text to the screen
+# Concatenate the generated chunks and print to screen
+text = ''.join(chunks)
 print(text)
