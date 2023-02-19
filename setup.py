12623005f45e6f31496fa3ca5ceeac4f663fc50d
Jonathan Gonzalez <jgonzalez249@gmail.com>
2023-02-19 00:50:16 -0500

    updated setup.py

---
+++ setup.py
@@ -0,0 +1,5 @@
+import subprocess
+subprocess.run(["pip", "install", "gpt-2-simple"])
+subprocess.run(["pip", "install", "tracery"])
+subprocess.run(["pip", "install", "tensorflow"])
+subprocess.run(["pip", "install", "nltk"])
