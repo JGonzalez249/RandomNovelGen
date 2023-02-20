44d0d597ab907b87bbd05c1d76bb9dcd105d2fe0
JGonzalez249 <jgonzalez249@gmail.com>
2023-02-20 16:24:07 -0500

    rebased setup and README

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 import subprocess
 subprocess.run(["pip", "install", "gpt-2-simple"])
 subprocess.run(["pip", "install", "tracery"])
-subprocess.run(["pip", "install", "nltk"])
+subprocess.run(["pip", "install", "--user", "-U" "nltk"])
 
 import nltk
 nltk.download('punkt')
